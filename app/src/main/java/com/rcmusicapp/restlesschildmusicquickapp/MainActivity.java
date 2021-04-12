04c66a8706622103cbce51bb7e2d1c9a5a94ba95
cameramanbrayton <info@duelingworlds.com>
2021-04-12 00:06:20 -0500

    restlesschildmusicquickapp03

--- app/src/main/java/com/rcmusicapp/restlesschildmusicquickapp/MainActivity.java
+++ app/src/main/java/com/rcmusicapp/restlesschildmusicquickapp/MainActivity.java
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.homeRcLinkText01);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public void openBrowser(View view){
