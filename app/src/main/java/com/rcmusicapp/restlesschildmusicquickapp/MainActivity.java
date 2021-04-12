f8a4a7e5d2e606ef594479e40a210d7758a70b8e
cameramanbrayton <info@duelingworlds.com>
2021-04-11 23:41:07 -0500

    restlesschildmusicquickapp02

--- app/src/main/java/com/rcmusicapp/restlesschildmusicquickapp/MainActivity.java
+++ app/src/main/java/com/rcmusicapp/restlesschildmusicquickapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.rcmusicapp.restlesschildmusicquickapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openBrowser(View view){
+
+        //Get url from tag
+        String url = (String)view.getTag();
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        //pass the url to intent data
+        intent.setData(Uri.parse(url));
+
+        startActivity(intent);
     }
 }
