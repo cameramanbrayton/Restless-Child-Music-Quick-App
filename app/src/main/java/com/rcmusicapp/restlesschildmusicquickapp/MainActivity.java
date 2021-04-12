4e1d1dc620c72d0669ce3c3a1f6549957ca1b4bd
cameramanbrayton <info@duelingworlds.com>
2021-04-12 13:49:18 -0500

    restlesschildmusicquickapp012

--- app/src/main/java/com/rcmusicapp/restlesschildmusicquickapp/MainActivity.java
+++ app/src/main/java/com/rcmusicapp/restlesschildmusicquickapp/MainActivity.java
@@ -30,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.duelingHomeLinkText);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        textView = findViewById(R.id.bgdHomeLinkText);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public void openBrowser(View view){
